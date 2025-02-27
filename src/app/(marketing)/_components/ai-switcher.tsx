e98496c252547ab1876184736aef7469aaffd0c2
psantosh16 <santoshphadtare1610@gmail.com>
2025-02-28 03:22:16 +0530

    refactor: Standardize string quotes and improve formatting in various components; enhance hero text and counter display

--- src/app/(marketing)/_components/ai-switcher.tsx
+++ src/app/(marketing)/_components/ai-switcher.tsx
@@ -1,33 +1,52 @@
-"use client";
+'use client';
 
-import { Intelligence } from "@/types/intelligence";
-import { cn } from "@/utils/cn";
-import { Switch } from "@headlessui/react";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { Intelligence } from '@/types/intelligence';
+import { cn } from '@/utils/cn';
+import { Switch } from '@headlessui/react';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
 export const Switcher = () => {
   const [enabled, setEnabled] = useState<Intelligence>(Intelligence.MISTRAL);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
-      localStorage.setItem("agent", enabled);
+      localStorage.setItem('agent', enabled);
     }, 300);
 
     return () => clearTimeout(timeoutId);
-  },[enabled]);
+  }, [enabled]);
 
   return (
-    <div  id="ai_model_switcher" className="flex gap-x-4 items-center justify-center">
-      <h3 className={cn(enabled === Intelligence.LLAMA ? "text-black font-semibold" : "text-black/40 opacity-60", "flex items-center gap-x-2")}>
-        <Image src="/icons/llama.webp"  alt="llama icon" className="w-8 md:w-12"  width={100} height={100}/>
+    <div
+      id="ai_model_switcher"
+      className="flex gap-x-4 items-center justify-center"
+    >
+      <h3
+        className={cn(
+          enabled === Intelligence.LLAMA
+            ? 'text-black font-semibold'
+            : 'text-black/40 opacity-60',
+          'flex items-center gap-x-2'
+        )}
+      >
+        <Image
+          src="/icons/llama.webp"
+          alt="llama icon"
+          className="w-8 md:w-12"
+          width={100}
+          height={100}
+        />
         llama 70b
       </h3>
       <Switch
         checked={enabled === Intelligence.MISTRAL}
         onChange={() => {
           setEnabled((prev) => {
-            const newValue = prev === Intelligence.LLAMA ? Intelligence.MISTRAL : Intelligence.LLAMA;
+            const newValue =
+              prev === Intelligence.LLAMA
+                ? Intelligence.MISTRAL
+                : Intelligence.LLAMA;
             return newValue;
           });
         }}
@@ -38,9 +57,22 @@ export const Switcher = () => {
           className="pointer-events-none inline-block h-5 w-5 translate-x-0 rounded-full bg-white ring-0 shadow-lg transition duration-200 ease-in-out group-data-[checked]:translate-x-7"
         />
       </Switch>
-      <h3 className={cn(enabled === Intelligence.MISTRAL ? "text-black font-semibold" : "text-black/40 opacity-60", "flex items-center gap-x-2")}>
+      <h3
+        className={cn(
+          enabled === Intelligence.MISTRAL
+            ? 'text-black font-semibold'
+            : 'text-black/40 opacity-60',
+          'flex items-center gap-x-2'
+        )}
+      >
         Mistral s24B
-      <Image src="/icons/mistral.webp"  alt="llama icon" className="w-6 md:w-8 "  width={100} height={100}/>
+        <Image
+          src="/icons/mistral.webp"
+          alt="llama icon"
+          className="w-6 md:w-8 "
+          width={100}
+          height={100}
+        />
       </h3>
     </div>
   );
--- src/types/github.ts
+++ src/types/github.ts
@@ -1,30 +1,29 @@
-
 export interface GithubUser {
-    login: string;
-    avatar_url: string;
-    html_url: string;
-    name: string | null;
-    company: string | null;
-    blog: string | null;
-    location: string | null;
-    email: string | null;
-    bio: string | null;
+  login: string;
+  avatar_url: string;
+  html_url: string;
+  name: string | null;
+  company: string | null;
+  blog: string | null;
+  location: string | null;
+  email: string | null;
+  bio: string | null;
 }
 
 export interface GithubRepo {
-    name: string;
-    description: string | null;
-    language: string | null | string[];
-    topics?: string[];
+  name: string;
+  description: string | null;
+  language: string | null | string[];
+  topics?: string[];
 }
 
 export interface ResponseStatus {
-    code: number;
-    message: string;
+  code: number;
+  message: string;
 }
 
 export interface GithubUserData {
-    user: GithubUser | null;
-    repos: GithubRepo[];
-    status: ResponseStatus;
+  user: GithubUser | null;
+  repos: GithubRepo[];
+  status: ResponseStatus;
 }
